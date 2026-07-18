9dee6386bf55af6c9bfb709e15cc757cba4e5c46
gartoon090910-ux <gartoon.090910@gmail.com>
2026-07-18 11:52:44 +0700

    Finish count Score

--- Assets/Script/Ball.cs
+++ Assets/Script/Ball.cs
@@ -1,5 +1,6 @@
 using System;
 using UnityEngine;
+using UnityEngine.EventSystems;
 public enum BallColor
 {
     White,
@@ -11,13 +12,20 @@ public enum BallColor
     Pink,
     Black
 }
-public class Ball : MonoBehaviour
+public class Ball : MonoBehaviour, IPointerClickHandler
 {
     [SerializeField]
     private int point;
 
     [SerializeField]
     private BallColor color;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Debug.Log(point);
+        GameManager.instance.PlayerScore += point;
+        Destroy(gameObject);
+    }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField]
     private int playerScore;
     public int PlayerScore { get { return playerScore; } set { playerScore = value; } }
 
