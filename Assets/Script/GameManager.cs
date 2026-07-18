f0db8bb652a97d66b62937bdc18831764992328d
gartoon090910-ux <gartoon.090910@gmail.com>
2026-07-18 11:38:07 +0700

    Referee System

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private int playerScore;
+    public int PlayerScore { get { return playerScore; } set { playerScore = value; } }
+
+    public static GameManager instance;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Awake()
+    {
+        instance = this;
+    }
+
+    // Update is called once per frame
+    private void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+}
