b146d84f42e27c4d760d25f4d234e4505371a13e
gartoon090910-ux <gartoon.090910@gmail.com>
2026-08-24 14:48:08 +0700

    save game

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -49,6 +49,9 @@ public class GameManager : MonoBehaviour
         SetBall(BallColor.Pink, 6);
         SetBall(BallColor.Black, 7);
 
+        if(Settings.fromSave)
+        LoadGame();
+
     }
     void Update()
     {
@@ -66,6 +69,9 @@ public class GameManager : MonoBehaviour
 
         if(Keyboard.current.backspaceKey.wasPressedThisFrame)
             StopBall();
+
+        if (Keyboard.current.leftShiftKey.isPressed && Keyboard.current.sKey.wasPressedThisFrame)
+            SaveGame();
     }
 
     private void SetBall(BallColor col, int i)
@@ -124,5 +130,33 @@ public class GameManager : MonoBehaviour
     public void ShowString(string s)
     {
     notiText.text = s;
+    }
+
+    public void SaveGame()
+    {
+        StopBall();
+        if (cueBall != null)
+        {
+        PlayerPrefs.SetFloat("cueBallPosX", cueBall.transform.position.x);
+        PlayerPrefs.SetFloat("cueBallPosY", cueBall.transform.position.y);
+        PlayerPrefs.SetFloat("cueBallPosZ", cueBall.transform.position.z);
+        Debug.Log("Saved");
+        }
+
+    }
+
+    public void LoadGame()
+    {
+        if (cueBall != null) 
+        {
+            float x = PlayerPrefs.GetFloat("cueBallPosX");
+            float y = PlayerPrefs.GetFloat("cueBallPosY");
+            float z = PlayerPrefs.GetFloat("cueBallPosZ");
+
+            cueBall.transform.position = new Vector3(x, y, z);
+
+            Debug.Log("Loaded");
+
+        }
     }
 }
--- Assets/Script/SceneMenu.cs
+++ Assets/Script/SceneMenu.cs
@@ -5,7 +5,14 @@ public class SceneMenu : MonoBehaviour
 {
     public void Startgame()
     {
+        Settings.fromSave = false;
         SceneManager.LoadScene("Loading");  
+    }
+
+    public void LoadSavedNewGame()
+    {
+        Settings.fromSave = true;
+        SceneManager.LoadScene("Loading");
     }
 
     public void Exit()
